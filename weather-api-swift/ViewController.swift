a3cc51760935968b9e550fcbc77786f3d25c9fd1
yamatokawai <ykawai@system-renovate.co.jp>
2025-02-18 13:15:09 +0900

    Initial Commit

---
+++ weather-api-swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  weather-api-swift
+//
+//  Created by 川井やまと on 2025/02/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ weather-api-swiftTests/weather_api_swiftTests.swift
@@ -0,0 +1,17 @@
+//
+//  weather_api_swiftTests.swift
+//  weather-api-swiftTests
+//
+//  Created by 川井やまと on 2025/02/18.
+//
+
+import Testing
+@testable import weather_api_swift
+
+struct weather_api_swiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
